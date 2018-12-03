92af0885a3ffbe9ceaa227e93b0e4725c1f8d628
Anthony Palmer <anthonyp4312@gmail.com>
2018-12-03 01:23:57 -0500

    day 3 wip

---
+++ src/advent_of_code/dec_03.clj
@@ -0,0 +1,29 @@
+(ns advent-of-code.dec-03
+  (:require [clojure.string :as str]
+            [advent-of-code.input :refer [day3] :rename {day3 input}]))
+
+;; PART 1
+;; Part find the number of overlapping squares
+(def matrix (vec (for [_ (repeat 1000 0)] (vec (repeat 1000 0)))))
+
+(defn parse-claim
+  "Given a claim-string return the parsed values"
+  [s]
+  (let [[id _ coords size] (str/split s #" ")
+        [x y] (map read-string (str/split (str/replace coords #":" "") #","))
+        [w h] (map read-string (str/split size #"x"))]
+    {:id id
+     :x x
+     :y y
+     :width w
+     :height h}))
+
+(defn apply-claim
+  "Given a claim, increment the appropriate coordinates in the matrix"
+  [m {:keys [id x y width height]}]
+  (println id)
+  (let [coords (for [a (range x (+ x width)) b (range y (+ y height))] [a b])]
+    (reduce #(update-in m [width height] inc) m coords)
+    ))
+
+(def claimed-matrix (reduce apply-claim matrix (take 3 (map parse-claim input))))
